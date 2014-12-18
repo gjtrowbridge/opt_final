3451a1e6c67387e55ea0dc653d644799c00054e2
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-12-18 03:01:18 -0500

    Remove test code

--- final_exam_part1c.R
+++ final_exam_part1c.R
@@ -4,10 +4,8 @@ part1c = function(x, A, b) {
   u = rep(0, n)
   for (i in 1:20) {
     direction = get_descent_direction(u, x, A, b)
-    step_size = armijos_step_size(u, x, A, b, 0.5, direction, tau = 0.5, c = 0.5)
-    u = u - direction * 0.001
-    print(get_loss(u,x,A,b))
-    print(u)
+    step_size = 0.001
+    u = u - direction * step_size
   }
   return(u)
 }
@@ -41,22 +39,3 @@ get_descent_direction = function(u, x, A, b) {
   
   return(new_deriv + regular_deriv)
 }
-
-armijos_step_size = function(u, x, A, b, max_step_size, gradient, tau = 0.5, c = 0.5) {
-  m = gradient %*% -gradient
-  t = -c * m
-  step_size = max_step_size
-  while (get_loss(u, x, A, b) - get_loss(u - step_size * gradient, x, A, b) < step_size * t) {
-    step_size = step_size * tau
-  }
-  return(step_size)
-}
-
-m = 5
-n = 3
-x = rep(1, n)
-A = mvrnorm(m, rep(0, n), diag(rep(1, n)))
-A = matrix(rep(3, n*m), nrow=m)
-b = rep(2, m)
-
-part1c(x, A, b)
